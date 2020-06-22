a8b5a27db321afc7c79c8bbefdf8787c4ac9d701
Slava Ulianov <sulianov@Slavas-MacBook-Pro.local>
2020-06-22 23:17:58 +0300

    added average calculation

--- scripts.js
+++ scripts.js
@@ -6,20 +6,20 @@ document.querySelector("#search-btn").addEventListener("click", function () {
 	searchData.state = document.querySelector("#state").value;
 	searchData.country = document.querySelector("#country").value;
 });
+var iqair = {
+	name: "iqair.com",
+	url: "https://api.airvisual.com/v2",
+	key: "edc2772e-2265-4a2f-a329-b99922199cd6",
+};
+
+var aqi = {
+	name: "aqicn.org",
+	url: "https://api.waqi.info/feed/",
+	key: "8d06a01f6a4d9bcd42ea4bc38433cd2c09da0803",
+};
 
 document.querySelector("#search-btn").addEventListener("click", function () {
 	// call api
-	var iqair = {
-		name: "iqair.com",
-		url: "https://api.airvisual.com/v2",
-		key: "edc2772e-2265-4a2f-a329-b99922199cd6",
-	};
-
-	var aqi = {
-		name: "aqicn.org",
-		url: "https://api.waqi.info/feed/",
-		key: "8d06a01f6a4d9bcd42ea4bc38433cd2c09da0803",
-	};
 
 	var city = searchData.city;
 	var state = searchData.state;
@@ -47,9 +47,6 @@ document.querySelector("#search-btn").addEventListener("click", function () {
 				});
 			document.querySelector("#iqair").innerHTML =
 				city + "'s" + " AQI from iqair.com is: " + iqair.pollution.aqius;
-			console.log(
-				city + "'s" + " AQI from iqair.com is: " + iqair.pollution.aqius
-			);
 		} else if (source === "aqi") {
 			await fetch(aqi.url + city + "/?token=" + aqi.key)
 				.then((res) => res.json())
@@ -61,10 +58,17 @@ document.querySelector("#search-btn").addEventListener("click", function () {
 				});
 			document.querySelector("#aqicn").innerHTML =
 				city + "'s" + " AQI from aqicn.org is: " + aqi.pollution.aqi;
-			console.log(city + " AQI from aqicn.org is: " + aqi.pollution.aqi);
 		}
+		//console.log(getAverageAQI(iqair.pollution.aqius, aqi.pollution.aqi));
+		document.querySelector("#average").innerHTML =
+			"Average AQI is: " + (aqi.pollution.aqi + iqair.pollution.aqius) / 2;
 	}
 
 	getData("iqair");
 	getData("aqi");
 });
+
+/*function getAverageAQI(aqi1, aqi2) {
+	var average = (aqi1 + aqi2) / 2;
+	return average;
+}*/
